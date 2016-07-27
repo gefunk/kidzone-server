b5f9f3763e0427500e051790b702c7d3d30547e8
gefunk <rahul.gokulnath@gmail.com>
2016-07-27 15:42:49 -0400

    App folder

---
+++ app/model/customer.js
@@ -0,0 +1,23 @@
+
+
+const mongoose = require('mongoose');
+
+const CustomerSchema = new Schema({
+  first_name: { type : String, default : '', trim : true },
+  last_name: { type : String, default : '', trim : true },
+  comments: [{
+    body: { type : String, default : '' },
+    user: { type : Schema.ObjectId, ref : 'User' },
+    createdAt: { type : Date, default : Date.now }
+  }],
+  image: {
+    cdnUri: String,
+    files: []
+  },
+  createdAt  : { type : Date, default : Date.now }
+});
+
+CustomerSchema.path('first_name').required(true, 'First name cannot be blank');
+CustomerSchema.path('last_name').required(true, 'Last Name cannot be blank');
+
+mongoose.model('Customer', CustomerSchema);
